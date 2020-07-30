2446f96280c7703bebff86837ed8d11a5a43bcfc
ericjmossman@gmail.com <ericjmossman@gmail.com>
2020-07-30 12:21:11 -0400

    Add license

--- rc6672/Constant.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import RealmSwift
-
-struct Constants {
-    static let REALM_APP_ID = "<APP_ID>"
-    static let LOCAL_PATH = Bundle.main.url(forResource: "bundled", withExtension: "realm")
-}
--- rc6672/Constants.swift
+++ rc6672/Constants.swift
@@ -19,21 +19,7 @@
 import Foundation
 import RealmSwift
 
-// Class definition from 'Grade' in realm-cocoa #6672
-// This class is just used in a local realm.
-class LocalClass: Object {
-    @objc dynamic var _id=""
-    @objc dynamic var sno=0
-    @objc dynamic var name=""
-    @objc dynamic var version=0
-    @objc dynamic var timestamp = Date()
-    let linkedPrimarySkills = List<String>()
-    
-    override static func primaryKey() -> String? {
-        return "_id"
-    }
-    
-    override static func indexedProperties() -> [String] {
-        return ["sno", "name"]
-    }
+struct Constants {
+    static let REALM_APP_ID = "<APP_ID>"
+    static let LOCAL_PATH = Bundle.main.url(forResource: "bundled", withExtension: "realm")
 }
